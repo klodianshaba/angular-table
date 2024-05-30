86108c5e0d1da938c12891e5064f339ecb6e2f05
klodian shaba <dev.klodian@gmail.com>
2024-05-30 20:23:32 +0200

    got demo out from home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./pages/welcome/welcome.component').then(m => m.WelcomeComponent) },
+  { path: 'demo', loadComponent: () => import('./pages/demo/demo.component').then(m => m.DemoComponent) },
+];
