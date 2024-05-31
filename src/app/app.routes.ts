e674023e76e1c21089dfc934e8933bf526ad7409
klodian shaba <dev.klodian@gmail.com>
2024-05-31 11:01:08 +0200

    redirect to welcome from unknown route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/welcome/welcome.component').then(m => m.WelcomeComponent) },
   { path: 'demo', loadComponent: () => import('./pages/demo/demo.component').then(m => m.DemoComponent) },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
